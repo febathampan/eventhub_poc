b1f0821bff3c81423995377f17850fd542be78c1
febathampan <fthampan3147@conestogac.on.ca>
2024-07-16 10:42:34 -0400

    initial commit

---
+++ src/main/java/com/feba/eventhubpoc/EventhubpocApplication.java
@@ -0,0 +1,13 @@
+package com.feba.eventhubpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubpocApplication.class, args);
+	}
+
+}
